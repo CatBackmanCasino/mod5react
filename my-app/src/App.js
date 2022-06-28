8b2df709a05a16185cc0a9aa00ba8c11a6b3686c
CatBackmanCasino <eric.aj.backman@gmail.com>
2022-06-28 14:48:40 +0000

    finished REACT essentials

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import HTTPPost from './components/HTTPPost';
+import HTTPHooks from './components/HTTPHooks';
 
 
 
@@ -8,7 +8,7 @@ import HTTPPost from './components/HTTPPost';
 function App() {
   return (
     <div className="App">
-      <HTTPPost />
+      <HTTPHooks />
     </div>
   );
 }
---
+++ my-app/src/Secrets.js
@@ -0,0 +1,3 @@
+const API_KEY = "28325732-2294b99e979ba160069d4cf5e";
+
+export default API_KEY
